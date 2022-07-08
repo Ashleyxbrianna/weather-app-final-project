5362051b98ebfe641d412fb81f5d3ea7bd284a99
Ashley <ashleyxcodes@gmail.com>
2022-07-08 15:17:33 -0700

    updated javascript

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
 let apiKey = "c3f7a97840ffea602b1dcea01b294765";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid={apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 
 console.log(apiUrl);
