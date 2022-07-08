a55b1179f1eb8d690822f1e1df77bc93b937a3e4
Ashley <ashleyxcodes@gmail.com>
2022-07-08 16:29:30 -0700

    updated the city

--- src/app.js
+++ src/app.js
@@ -38,6 +38,7 @@ function displayTemperature(response) {
 }
 
 let apiKey = "c3f7a97840ffea602b1dcea01b294765";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
